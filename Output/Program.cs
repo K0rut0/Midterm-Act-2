033c324600c4f8492c24d20e46be1a13f89f914c
Koruto <101617874+K0rut0@users.noreply.github.com>
2024-04-03 19:05:59 +0800

    Add files via upload

---
+++ Output/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using trig;
+
+Trigonometrics TriManager = new Trigonometrics();
+
+int st = 0;
+while (st == 0)
+{
+    Console.WriteLine(
+        "[1] Compute the opposite side using Sine\n[2] Compute the opposite side using Tan\n" +
+        "[3] Compute Hypotenuse using Sine\n[4] Compute Hypotenuse using Cosine\n" +
+        "[5] Compute adjacent side using Cosine\n[6] Compute adjacent side using Tan\n" +
+        "[7] Compute angle using sine\n[8] Compute angle using cos\n[9] Compute angle using tan\n" +
+        "[0] Compute radians based on opposite side and hypotenuse\n" +
+        "[11] Compute radians based on angle\n" +
+        "[12] Exit"
+        );
+    Console.Write("Enter opperation: ");
+    int n = int.Parse( Console.ReadLine() );
+    switch (n)
+    {
+        case 1:
+            Console.WriteLine("Output = " + TriManager.ComputeOppSin());
+            break;
+        case 2:
+            Console.WriteLine("Output = " + TriManager.ComputeOppTan());
+            break;
+        case 3:
+            Console.WriteLine("Output = " + TriManager.ComputeHypSin());
+            break;
+        case 4:
+            Console.WriteLine("Output = " + TriManager.ComputeHypCos());
+            break;
+        case 5:
+            Console.WriteLine("Output = " + TriManager.ComputeAdjCos());
+            break;
+        case 6:
+            Console.WriteLine("Output = " + TriManager.ComputeAdjTan());
+            break;
+        case 7:
+            Console.WriteLine("Output = " + TriManager.ComputeAngleSin());
+            break;
+        case 8:
+            Console.WriteLine("Output = " + TriManager.ComputeAngleCos());
+            break;
+        case 9:
+            Console.WriteLine("Output = " + TriManager.ComputeAngleTan());
+            break;
+        case 0:
+            Console.WriteLine("Output = " + TriManager.ComputeRadSin());
+            break;
+        case 11:
+            Console.WriteLine("Output = " + TriManager.PromptDegToRad());
+            break;
+        case 12:
+            st++;
+            break;
+
+    }
+        
+}
---
+++ trig/Trignometrics.cs
@@ -0,0 +1,103 @@
+﻿namespace trig
+{
+    public class Trigonometrics
+    {
+        public double DegToRad(double Angle)
+        {
+            return (Angle * (Math.PI / 180));
+        }
+        public double RadToDeg(double Rad)
+        {
+            double Angle = (Rad * 180)/Math.PI;
+            return Angle;
+        }
+        public double ComputeOppSin()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the hypotenuse: ");
+            double HypLength = Double.Parse(Console.ReadLine());
+            return Math.Sin(Angle) * HypLength;
+        }
+        public double ComputeOppTan()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the adjacent side: ");
+            double AdjLength = Double.Parse(Console.ReadLine());
+            return Math.Tan(Angle) * AdjLength;
+        }
+        public double ComputeHypSin()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the opposite side: ");
+            double OppLength = Double.Parse(Console.ReadLine());
+            return OppLength/Math.Sin(Angle);
+        }
+        public double ComputeHypCos()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the adjacent side: ");
+            double AdjLength = Double.Parse(Console.ReadLine());
+            return AdjLength/Math.Cos(Angle);
+        }
+        public double ComputeAdjCos()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the hypotenuse: ");
+            double HypLength = Double.Parse(Console.ReadLine());
+            return Math.Cos(Angle) * HypLength;
+        }
+        public double ComputeAdjTan() 
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            Console.Write("Enter the length of the opposite side: ");
+            double OppLength = Double.Parse(Console.ReadLine());
+            return OppLength/Math.Tan(Angle);
+        }
+        public double ComputeAngleSin()
+        {
+            Console.Write("Enter the length of the opposite side: ");
+            double OppLength = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of the hypotenuse: ");
+            double HypLength = Double.Parse(Console.ReadLine());
+            return RadToDeg(Math.Asin(OppLength / HypLength));
+        }
+        public double ComputeAngleCos()
+        {
+            Console.Write("Enter the length of the adjacent side: ");
+            double AdjLength = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of the hypotenuse: ");
+            double HypLength = Double.Parse(Console.ReadLine());
+            return RadToDeg(Math.Acos(AdjLength / HypLength));
+        }
+        public double ComputeAngleTan()
+        {
+            Console.Write("Enter the length of the opposite side: ");
+            double OppLength = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of the adjacent side: ");
+            double AdjLength = Double.Parse(Console.ReadLine());
+            return RadToDeg(Math.Atan(OppLength / AdjLength));
+        }
+        public double ComputeRadSin()
+        {
+            Console.Write("Enter the length of the opposite side: ");
+            double OppLength = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of the hypotenuse: ");
+            double HypLength = Double.Parse(Console.ReadLine());
+            double Angle = Math.Asin(OppLength / HypLength);
+            return DegToRad(Angle);
+        }
+        public double PromptDegToRad()
+        {
+            Console.Write("Enter the angle in degrees: ");
+            double Angle = DegToRad(Double.Parse(Console.ReadLine()));
+            return Angle;
+        }
+
+    }
+}
